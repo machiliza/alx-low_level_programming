b4ca2bad0c6dff70903a369f2eeb7f0a761f82ef
machiliza <pinkynolwa@gmail.com>
2023-07-13 07:24:02 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program thats prints a line with puts fuction
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
