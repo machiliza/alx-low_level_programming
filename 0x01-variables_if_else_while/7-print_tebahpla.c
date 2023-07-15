02c3e11f89acded8d0c75d3d3a669970c0f07169
machiliza <pinkynolwa@gmail.com>
2023-07-15 16:45:44 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,25 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
  * main - entry point
  *
- * Return:always return 0
+ * Return: alway returns 0
  *
  */
 int main(void)
 {
-	int n;
+	char a;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	} else
-	{
-		printf("%d is positive\n", n);
-	}
+	for (a = 'z'; a >= 'a' ; a--)
+		putchar(a);
+	putchar('\n');
 	return (0);
 }
