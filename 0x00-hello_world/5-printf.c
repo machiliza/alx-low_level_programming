d853c3b1934ae2cf14ab0e1b8e0b2c7d97108579
machiliza <pinkynolwa@gmail.com>
2023-07-16 12:44:52 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: always 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }	
 	 
